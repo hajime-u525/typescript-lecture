298d9824ddb38c281ad628aca28e340d37a11342
hajime_ueno <kanriaddress0@gmail.com>
2026-06-23 20:22:26 +0900

    メモの追加

--- src/enemy.ts
+++ src/enemy.ts
@@ -7,11 +7,11 @@ export class Enemy extends Character {
     super(name, hp);
     this.maxHp = hp;
   }
-
+  //プロパティの初期化
   getHpRatio(): number {
     return this.hp / this.maxHp;
   }
-
+  //ダメージを受ける
   takeDamage(damage: number): void {
     this.hp -= damage;
 
@@ -19,7 +19,7 @@ export class Enemy extends Character {
       this.hp = 0;
     }
   }
-
+  //通常時：name +"は攻撃してきた！"、HPが30%以下の時：name + "は必死に抵抗している！"
   attack(): void {
     if (this.getHpRatio() <= 0.3) {
       console.log(`${this.name}は必死に抵抗している！`);
