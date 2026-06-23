7b1479d2cf5f9c0ca10fbd97c6e27ac80cfb1712
hajime_ueno <kanriaddress0@gmail.com>
2026-06-23 21:40:23 +0900

    抽象メソッドの形を abstract attack(opponent: Character): void に変更する

--- src/enemy.ts
+++ src/enemy.ts
@@ -11,14 +11,7 @@ export class Enemy extends Character {
   getHpRatio(): number {
     return this.hp / this.maxHp;
   }
-  //ダメージを受ける
-  takeDamage(damage: number): void {
-    this.hp -= damage;
 
-    if (this.hp < 0) {
-      this.hp = 0;
-    }
-  }
   //通常時：name +"は攻撃してきた！"、HPが30%以下の時：name + "は必死に抵抗している！"
   attack(): void {
     if (this.getHpRatio() <= 0.3) {
